c88e1f243e2eeaf921a551e448a5e7f6065e2c9e
testacademy2024 <sanketumrani14041992@gmail.com>
2025-08-05 23:44:45 +0530

    Initial commit

---
+++ src/ex_05082025/Lab003.java
@@ -0,0 +1,13 @@
+public class Lab003 {
+    public static void main(String[] args) {
+        //int a=10;   //Single Line Comment
+
+        /*
+        This is a MultiLine Comment
+
+
+         */
+
+    }
+
+}
