26489b509051d93a8fb393c42bcae58021cf0846
testacademy2024 <sanketumrani14041992@gmail.com>
2025-08-06 23:49:48 +0530

    This Week Task - Week#2 - ATBX(06-08-2025)

---
+++ src/ex_06082025/Lab019.java
@@ -0,0 +1,8 @@
+package ex_06082025;
+
+public class Lab019 {
+    public static void main(String[] args) {
+
+        //Unicode characters Multiple examples
+    }
+}
---
+++ src/ex_06082025/Lab024.java
@@ -0,0 +1,9 @@
+package ex_06082025;
+
+public class Lab024 {
+    public static void main(String[] args) {
+        int n = 9;
+        System.out.printf("%d",n);
+
+    }
+}
